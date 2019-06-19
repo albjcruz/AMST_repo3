c6d7009565cd5806e37b00557252ac6a69ab50f8
Steven Santillan <steven_camo@hotmail.com>
2019-06-19 15:45:15 -0500

    Se agregaron las actividades para el mapa y calendario

--- app/src/main/java/com/example/estudiante/amst03/Calendario.java
+++ app/src/main/java/com/example/estudiante/amst03/Calendario.java
@@ -3,11 +3,11 @@ package com.example.estudiante.amst03;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Calendar extends AppCompatActivity {
+public class Calendario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_calendario);
     }
 }
--- app/src/main/java/com/example/estudiante/amst03/MainActivity.java
+++ app/src/main/java/com/example/estudiante/amst03/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.estudiante.amst03;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void AbrirView(View view) {
+
+    }
+
+    public void AbrirCalendario(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Calendario.class);
+                startActivity(intent);
+            }
+        }, 0);
+    }
+
+    public void AbrirMap(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Maps.class);
+                startActivity(intent);
+            }
+        }, 0);
+    }
+
+    public void AbrirGraph(View view) {
+    }
+
+    public void AbrirFlotante(View view) {
     }
 }
