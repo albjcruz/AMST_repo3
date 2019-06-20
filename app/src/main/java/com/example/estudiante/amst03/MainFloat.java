0aee05281a08ff674633e5dfdb1c111d6b9249b6
Steven Santillan <steven_camo@hotmail.com>
2019-06-20 17:25:27 -0500

    Se agrego el menu con el boton flotante

--- app/src/main/java/com/example/estudiante/amst03/MainFloat.java
+++ app/src/main/java/com/example/estudiante/amst03/MainFloat.java
@@ -2,7 +2,12 @@ package com.example.estudiante.amst03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import com.example.estudiante.amst03.Calendar.CalendarActivity;
 
 public class MainFloat extends AppCompatActivity {
 
@@ -10,5 +15,45 @@ public class MainFloat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_float);
+    }
+
+    public void RegresarAlMenu(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainFloat.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        }, 0);
+    }
+
+    public void AbrirMap(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainFloat.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        }, 0);
+    }
+
+    public void AbrirCalendario(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainFloat.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        }, 0);
+    }
+
+    public void AbrirVideo(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainFloat.this, activity_youtube.class);
+                startActivity(intent);
+            }
+        }, 0);
     }
 }
