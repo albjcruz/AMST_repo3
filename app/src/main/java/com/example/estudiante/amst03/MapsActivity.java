23194e8c6ffe30d8339bcfc86dc4546a66ee2662
Steven Santillan <steven_camo@hotmail.com>
2019-06-19 23:19:24 -0500

    Se agrego la opcion del mapa

--- app/src/main/java/com/example/estudiante/amst03/MapsActivity.java
+++ app/src/main/java/com/example/estudiante/amst03/MapsActivity.java
@@ -1,10 +1,20 @@
 package com.example.estudiante.amst03;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -12,15 +22,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
-   /* @Override
-  /*  protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }*/
+
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+        /**
+         * Valido que los servicios de google play esten activos
+         */
+        if (status== ConnectionResult.SUCCESS){
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }else{
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status,(Activity)getApplicationContext(),10);
+             }
+
+    }
 
 
     /**
@@ -35,10 +54,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        /**
+         * Elijo el tipo de mapa que deseo se muestre
+         */
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        UiSettings uiSettings= mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Add a marker in ESPOL and move the camera
+        LatLng Espol = new LatLng(-2.14994,-79.957681);
+        mMap.addMarker(new MarkerOptions().position(Espol).title("ESPOL").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        float zoomlevel=16;
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Espol,zoomlevel));
     }
 }
