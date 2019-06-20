d44887202a64cee06d00764ce95edb895031de04
Steven Santillan <steven_camo@hotmail.com>
2019-06-19 22:46:52 -0500

    Se agrego la api key

--- app/src/main/java/com/example/estudiante/amst03/Main2Activity.java
+++ app/src/main/java/com/example/estudiante/amst03/Main2Activity.java
@@ -1,6 +1,9 @@
 package com.example.estudiante.amst03;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,15 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void AbrirMap(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(Main2Activity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        }, 0);
     }
 }
